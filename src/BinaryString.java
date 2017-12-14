d6720e7694d1e3b97c0882d45eae0f9f97099802
PASSERproject <mpowell48@radford.com>
2017-12-13 23:11:08 -0500

    Now with Mendelian inheritance! Ones are dominant and zeros are recessive!

--- src/BinaryString.java
+++ src/BinaryString.java
@@ -1,22 +1,36 @@
 import java.util.Random;
 
 public class BinaryString {
-	private int[] bin;
+	private int[][] bin;
 	private int numOnes = 0;
 	private boolean allOnes = false;
 	
 	public BinaryString(int binStringSize) {
-		bin = new int[binStringSize];
+		bin = new int[binStringSize][2];
 		
 		Random r = new Random();
 		
 		for (int i = 0; i < binStringSize; i++) {
-			if(r.nextDouble() < .01) {
-				bin[i] = 1;
+			double chance = r.nextDouble();
+			
+			if(chance < .25) {
+				bin[i][0] = 1;
+				bin[i][1] = 1;
 				numOnes++;
 			}
-			else {
-				bin[i] = 0;
+			else if(chance >= .25 && chance < .5) {
+				bin[i][0] = 1;
+				bin[i][1] = 0;
+				numOnes++;
+			}
+			else if(chance >= .5 && chance < .75) {
+				bin[i][0] = 0;
+				bin[i][1] = 1;
+				numOnes++;
+			}
+			else if(chance >= .75 && chance < 1) {
+				bin[i][0] = 0;
+				bin[i][1] = 0;
 			}
 		}
 				
@@ -25,23 +39,42 @@ public class BinaryString {
 	
 	public BinaryString(BinaryString parent1, BinaryString parent2, double mutationChance) {
 		if(parent1.getBinLength() == parent2.getBinLength()) {
-			bin = new int[parent1.getBinLength()];
+			bin = new int[parent1.getBinLength()][2];
 			
 			Random r = new Random();
 			
 			for (int i = 0; i < this.getBinLength(); i++) {
-				if(r.nextBoolean()) {
-					bin[i] = parent1.getBinIthIndex(i);
+				double chance = r.nextDouble();
+				
+				if(chance < .25) {
+					bin[i][0] = parent1.getBinIthFirstAllel(i);
+					bin[i][1] = parent2.getBinIthFirstAllel(i);
 					
-					if(bin[i] == 1) {
+					if(bin[i][0] == 1 || bin[i][1] == 1) {
 						numOnes++;
 					}
 				}
-				
-				else {
-					bin[i] = parent2.getBinIthIndex(i);
+				else if(chance >= .25 && chance < .5) {
+					bin[i][0] = parent1.getBinIthFirstAllel(i);
+					bin[i][1] = parent2.getBinIthSecondAllel(i);
 					
-					if(bin[i] == 1) {
+					if(bin[i][0] == 1 || bin[i][1] == 1) {
+						numOnes++;
+					}
+				}
+				else if(chance >= .5 && chance < .75) {
+					bin[i][0] = parent1.getBinIthSecondAllel(i);
+					bin[i][1] = parent2.getBinIthFirstAllel(i);
+					
+					if(bin[i][0] == 1 || bin[i][1] == 1) {
+						numOnes++;
+					}
+				}
+				else {
+					bin[i][0] = parent1.getBinIthSecondAllel(i);
+					bin[i][1] = parent2.getBinIthSecondAllel(i);
+					
+					if(bin[i][0] == 1 || bin[i][1] == 1) {
 						numOnes++;
 					}
 				}
@@ -62,17 +95,34 @@ public class BinaryString {
 		
 		for(int i = 0; i < bin.length; i++) {
 			double d = r.nextDouble();
+			boolean currentlyExpressed = bin[i][0] == 1 || bin[i][1] == 1;
 		
 			if(d < mutationChance) {
-				if(bin[i] == 1) {
-					bin[i] = 0;
-					numOnes--;
+				if(bin[i][0] == 1) {
+					bin[i][0] = 0;
 				}
-				
 				else {
-					bin[i] = 1;
-					numOnes++;
+					bin[i][0] = 1;
 				}
+			}
+			
+			d = r.nextDouble();
+			
+			if(d < mutationChance) {
+				if(bin[i][1] == 1) {
+					bin[i][1] = 0;
+				}
+				else {
+					bin[i][1] = 1;
+				}
+			}
+			
+			if(currentlyExpressed == false && (bin[i][0] == 1 || bin[i][1] == 1)) {
+				numOnes++;
+			}
+			
+			if(currentlyExpressed == true && (bin[i][0] == 0 && bin[i][1] == 0)) {
+				numOnes--;
 			}
 		}
 	}
@@ -81,19 +131,44 @@ public class BinaryString {
 		for(int i = 0; i < bin.length; i++) {
 			allOnes = true;
 			
-			if(bin[i] == 0) {
+			if(bin[i][0] == 0 && bin[i][1] == 0) {
 				allOnes = false;
 				i = bin.length;
 			}
 		}
 	}
 	
+	@Override
+	public String toString() {
+		String s = "";
+		
+		for(int i = 0; i < bin.length; i++) {
+			s = s + bin[i][0];
+		}
+		
+		s = s + "\n";
+		
+		for(int i = 0; i < bin.length; i++) {
+			s = s + bin[i][1];
+		}
+		
+		s = s + "\n";
+		s = s + "numOnes: " + numOnes + "\n";
+		s = s + "allOnes: " + allOnes + "\n";
+		
+		return s;
+	}
+	
 	public int getBinLength() {
 		return bin.length;
 	}
 	
-	public int getBinIthIndex(int i) {
-		return bin[i];
+	public int getBinIthFirstAllel(int i) {
+		return bin[i][0];
+	}
+	
+	public int getBinIthSecondAllel(int i) {
+		return bin[i][1];
 	}
 	
 	public int getNumOnes() {
